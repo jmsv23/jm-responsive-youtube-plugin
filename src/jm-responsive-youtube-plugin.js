02d1b481020d932e31ddc836ef02643a61bc8a36
jose manuel santibañez villanueva <jmsv16@hotmail.com>
2013-10-20 12:42:36 -0500

    add src

---
+++ src/jm-responsive-youtube-plugin.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $.fn.jm-responsive-youtube-plugin = function (options) {
+    	var settings = $.extend({
+    		opt1    :    1,
+    		opt2    :    null,
+    	}, options);
+
+    	return this.each( function () {
+    		
+    	});
+    }
+}) (jQuery);
